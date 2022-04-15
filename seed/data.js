881dcef32ccafcf795e62d9617023b8710d3ef23
George Chen <georgechen1338@gmail.com>
2022-04-15 17:00:49 -0400

    test

--- seed/data.js
+++ seed/data.js
@@ -2,7 +2,7 @@ import db from "../db/connection.js";
 import Article from "../models/article.js";
 import Memory from "../models/memory.js";
 import memories from "./memories.json" assert { type: "json" };
-import { data } from "../testArticle.js" assert { type: "json" };
+import { data } from "../testArticle.js";
 // import allArticles from "../AllInOne.json" assert { type: "json" };
 
 const insertData = async () => {
