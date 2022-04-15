09266baa50c83a73040f466489ca8df20bb508b0
George Chen <georgechen1338@gmail.com>
2022-04-15 16:06:34 -0400

    r

--- seed/data.js
+++ seed/data.js
@@ -2,17 +2,18 @@ import db from "../db/connection.js";
 import Article from "../models/article.js";
 import Memory from "../models/memory.js";
 import memories from "./memories.json" assert { type: "json" };
-import articles from "./articles.json" assert { type: "json" };
+import allArticles from "../AllInOne.json" assert { type: "json" };
 
 const insertData = async () => {
-
-  await db.dropDatabase();
-
-  await Article.insertMany(articles);
+  await Memory.deleteMany();
 
   await Memory.insertMany(memories);
-
-  db.close();
 };
 
 insertData();
+// const insertNewData = async () => {
+//   await Article.deleteMany();
+//   await Article.insertMany(allArticles);
+//   db.close();
+// };
+// insertNewData();
