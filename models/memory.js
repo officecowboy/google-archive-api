ee0b156578abdb78e00b5287d1d953c23eb5e0c2
Michel Lizardo <michel@Michels-MacBook-Pro.local>
2022-04-15 11:17:58 -0400

    Updated back-end

--- models/memory.js
+++ models/memory.js
@@ -2,10 +2,10 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 let Memory = new Schema({
-  id: "",
-  Name: String,
-  Text: String,
-  Time: ""
-});
+  name: String,
+  text: String,
+},
+  { timestamps: true }
+);
 
 export default mongoose.model("memories", Memory);
--- seed/data.js
+++ seed/data.js
@@ -1,13 +1,12 @@
-import memories from "../controllers/memories.js";
 import db from "../db/connection.js";
 import Article from "../models/article.js";
 import Memory from "../models/memory.js";
 import memories from "./memories.json" assert { type: "json" };
-import articles from "./article.json" assert { type: "json" };
+import articles from "./articles.json" assert { type: "json" };
 
 const insertData = async () => {
 
-  db.dropDatabase();
+  await db.dropDatabase();
 
   await Article.insertMany(articles);
 
