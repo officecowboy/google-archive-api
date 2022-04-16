bd117f1f861755c226224f4c171be2c9c6a8d241
George Chen <georgechen1338@gmail.com>
2022-04-16 18:04:31 -0400

    fixing doodles code

--- seed/data.js
+++ seed/data.js
@@ -1,7 +1,7 @@
 import db from "../db/connection.js";
 import Article from "../models/article.js";
 import Memory from "../models/memory.js";
-import Doodles from "../models/googleDoodle.js";
+import Doodles from "../models/doodles.js";
 import Weather from "../models/weather.js";
 import memories from "./memories.json" assert { type: "json" };
 import allArticles from "../AllInOne.json" assert { type: "json" };
