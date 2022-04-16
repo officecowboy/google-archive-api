8559326b8bea55ac3a414ca5beb31909aa516935
George Chen <georgechen1338@gmail.com>
2022-04-16 17:44:44 -0400

    fixed index.js

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,18 @@
 import { Router } from "express";
 import articlesRoutes from "./articles.js";
 import memoriesRoutes from "./memories.js";
+import weatherRoutes from "./weather.js";
+import doodlesRoutes from "./doodles.js";
 
 const router = Router();
 
 router.get("/", (req, res) => res.send("This is the api root!"));
 
 router.use("/", articlesRoutes);
+
+router.use("/", weatherRoutes);
+
+router.use("/", doodlesRoutes);
 
 router.use("/", memoriesRoutes);
 
