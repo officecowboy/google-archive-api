6607f1e5d2dd05d460545e54cfba8e0d655dfef7
Scott Diekema <scott.diekema@gmail.com>
2022-06-21 08:53:28 -0400

    add cors headers

--- server.js
+++ server.js
@@ -11,8 +11,14 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors());
 app.use(logger("dev"));
-
 app.use("/api", routes);
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
 
 db.on("connected", () => {
   console.clear();
